2b83843a600802f5556f1ce174c678448895dfe1
Prakash Shrestha <prakashrestha2075@gmail.com>
2024-05-08 07:21:29 +0545

    ✨ Feat: added search feature

--- src/controllers/showAllJobs.js
+++ src/controllers/showAllJobs.js
@@ -2,7 +2,16 @@ const conn = require("../db/connection");
 
 const getAllJobs = async (req, res) => {
   try {
-    const selectQuery = "SELECT * FROM joblisting";
+    // Extracting search query from request query parameters
+    const { query } = req.query;
+
+    let selectQuery = "SELECT * FROM joblisting";
+
+    // If there's a search query, add it to the SQL query
+    if (query) {
+      selectQuery += ` WHERE jobTitle LIKE '%${query}%' OR companyName LIKE '%${query}%'`;
+    }
+
     conn.query(selectQuery, (err, result) => {
       if (err) {
         console.error("Error fetching job from db:", err);
@@ -11,8 +20,8 @@ const getAllJobs = async (req, res) => {
         if (result.length === 0) {
           res.status(404).send("Job not found");
         } else {
-          const job = result;
-          res.status(200).send(job);
+          const jobs = result;
+          res.status(200).send(jobs);
         }
       }
     });
