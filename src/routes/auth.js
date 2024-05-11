43d51e3cb07d5c2a2571c7f2f3c9af754df7328a
Rabidhau01 <np03cs4a220058@heraldcollege.edu.np>
2024-05-11 21:19:36 +0545

    "Update code for getting candidate info"

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,6 +6,8 @@ const { verifyToken } = require("../controllers/sendtoken"); // Import the verif
 const { verify } = require("../controllers/verify");
 const { info } = require("../controllers/getinfo");
 const { updateInfo } = require("../controllers/updateinfo");
+const { apply } = require("../controllers/apply");
+const { candy } = require("../controllers/getcandidate");
 
 router.route("/sign-up").post(signUp);
 router.route("/login").post(logIn);
@@ -13,4 +15,6 @@ router.route("/authentication").post(verifyToken); // Add the new route
 router.route("/verify-token").post(verify); 
 router.route("/user-profile/:userId").get(info); 
 router.route("/updateinfo/:userId").post(updateInfo); 
+router.route("/candidate/:userId").get(candy); 
+router.route("/update_candidate/:userId").post(apply); 
 module.exports = router;
